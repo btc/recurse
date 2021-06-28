610210870ab14a0f0acb5be37c0bb644c4dec006
btc <734339+btc@users.noreply.github.com>
2021-06-28 14:43:33 -0400

    update sdif

--- sdif/src/main.rs
+++ sdif/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use opencv::features2d::{SIFT, Feature2DTrait};
+
+fn main() -> opencv::Result<(), anyhow::Error> {
+    let mut sift = SIFT::create(1, 1, 1.0, 1.0, 1.0)?;
+    let mut keypoints = Vec::new();
+    let mask = Vec::new();
+    sift.detect(image, &mut keypoints, &mask)?;
+    Ok(())
 }
